1ddb9f5fdb2a1b4003bc284555c3a3bfdc952e09
cuongvu3009 <cuongvu3009@gmail.com>
2023-05-08 22:54:16 +0300

    add defaut

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, IntegerField, EmailField
+from wtforms import StringField, PasswordField, IntegerField, EmailField, BooleanField
 from wtforms.validators import InputRequired, Length
 
 class LoginForm(FlaskForm):
-   username = StringField('username', validators=[InputRequired(), Length(min=2, max=20)])
+   username = StringField('Your username', validators=[InputRequired(), Length(min=2, max=20)])
    password = PasswordField('password', validators=[InputRequired(), Length(min=6, max=20)])
    age = IntegerField('age', validators=[InputRequired()])
    email = EmailField('email', validators=[InputRequired()])
+   subscribe = BooleanField('Subscribe to news letter', default=True)
 
 def create_app():
     app = Flask(__name__)
@@ -18,7 +19,7 @@ def create_app():
       form = LoginForm()
 
       if form.validate_on_submit(): 
-         return f'<h1>Username: {form.username.data} | Password: {form.password.data} | Age: {form.age.data} | Email: {form.email.data}</h1>'
+         return f'<h1>Username: {form.username.data} | Password: {form.password.data} | Age: {form.age.data} | Email: {form.email.data} | Subscribe: {form.subscribe.data}</h1>'
       
       return render_template('index.html', form=form)
 
